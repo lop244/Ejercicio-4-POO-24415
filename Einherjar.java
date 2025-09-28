a1aa509ae93a0480f696a8845b22c183315df6c1
lop244 <lop24415@uvg.edu.gt>
2025-09-28 13:06:01 -0600

    imports y class

---
+++ Einherjar.java
@@ -0,0 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class Einherjar extends CombatienteNordico {
+    protected List<Item> inventario;
+    protected int capacidadItems;
