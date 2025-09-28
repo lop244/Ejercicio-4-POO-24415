df60807730ce1de890e5b9602e401248ca345357
lop244 <lop24415@uvg.edu.gt>
2025-09-28 12:28:39 -0600

    Update en clase

--- CombatienteNordico.java
+++ CombatienteNordico.java
@@ -1,3 +1,6 @@
-public class CombatienteNordico {
-    
-}
+import java.util.List;
+
+public abstract class CombatienteNordico {
+    protected String nombre;
+    protected int puntosDeVida;
+    protected int poderAtaque;
