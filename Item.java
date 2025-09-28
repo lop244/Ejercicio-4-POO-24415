7e40d94c9525bee830d1bb9b1d6f718e17e090fb
Luis P López <lop24415@uvg.edu.gt>
2025-09-28 11:58:56 -0600

    Update Item.java

--- Item.java
+++ Item.java
@@ -2,6 +2,11 @@ public class Item implements Utilizable {
     private String nombre;
     private String descripcion;
     private String canitdad;
+
+    @Override
+    public void usar(CombatienteNordico ejecutor, CombatienteNordico objetivo) {
+        
 }
+
 
 
