c5dabc091f1803b3c7ce3598a1679ec947700ca4
lop244 <lop24415@uvg.edu.gt>
2025-09-28 18:44:58 -0600

    get mensaje inicio

--- Skjaldmaer.java
+++ Skjaldmaer.java
@@ -8,3 +8,6 @@ public class Skjaldmaer extends Einherjar {
 @Override
 public void tomarTurno(List<CombatienteNordico> enemigos, List<CombatienteNordico> aliados) {
 }
+
+@Override
+public String getMensajeInicio() 
