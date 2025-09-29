fd89357fbee7c8f8fb4088bdcbfd0e45313e0234
lop244 <lop24415@uvg.edu.gt>
2025-09-28 18:54:30 -0600

    detalles de clase papa

--- CriaturaMitologica.java
+++ CriaturaMitologica.java
@@ -3,5 +3,7 @@ import java.util.List;
 public abstract class CriaturaMitologica extends CombatienteNordico {
     protected Habilidad habilidadPrimaria;
 
-    public
+    public CriaturaMitologica(String nombre, int vida, int ataque, Habilidad habilidad) {
+        super(nombre, vida, ataque);
+        this.habilidadPrimaria = habilidad;
 }
