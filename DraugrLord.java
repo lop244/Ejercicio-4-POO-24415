6caa2ef38a34af2f1981eec90f7e5bfb0288d2f1
lop244 <lop24415@uvg.edu.gt>
2025-09-28 19:11:58 -0600

    todos los stats  del personaje draugrlord

--- DraugrLord.java
+++ DraugrLord.java
@@ -4,7 +4,11 @@ public class DraugrLord extends Draugr {
 
     public DraugrLord() {
         super();
-        
+        this.nombre = "Dios y señor de los Draugr";
+        this.puntosDeVida = 200;
+        this.poderAtaque = 20;
+        this.habilidadDeJefe = new Habilidad();
+
     }
     
 }
